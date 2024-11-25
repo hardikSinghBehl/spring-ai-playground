2cca2db3b4df6adb90a8c2d90249cfe41c6df2dc
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-11-25 20:16:39 +0530

    remove command line runner

--- image-generator/src/main/java/com/behl/imagegen/Application.java
+++ image-generator/src/main/java/com/behl/imagegen/Application.java
@@ -1,24 +1,13 @@
 package com.behl.imagegen;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-    
-    @Autowired
-    private ImageGenerator imageGenerator;
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        var response = imageGenerator.generate("Cartoon monkey eating an apple");
-        System.out.println(response);
     }
 
 }
